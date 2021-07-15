c9682e14ace5e7877b815ea9aae2c91d6a7572cb
eduardonk9999 <thiago.novak7@gmail.com>
2021-07-15 09:54:02 -0300

    :lipstick: Add Width to body

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,11 +1,14 @@
+import './style.css';
 import CardGame from '../../components/CardGame'
 
 function BoardGame(amountCards) {
   const $htmlCardGame = CardGame()
-  const $htmlBoardGame = $htmlCardGame.repeat(amountCards)
-
-  console.log($htmlBoardGame)
-  return $htmlBoardGame
+  const $htmlContent = $htmlCardGame.repeat(amountCards)
+  return `
+    <section class="board-game">
+      ${$htmlContent}
+    </section>
+  `
 }
 
 export default BoardGame;
